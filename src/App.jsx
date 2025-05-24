b3f76a7072b4281ccad0940dbb4220ec73f32112
Girija-Kangutkar <kangutkargirija075@gmail.com>
2025-05-25 00:13:00 +0530

    Pagination of rick and morty api

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import RickAndMorty from "./components/RickAndMorty";
+
+function App() {
+  return (
+    <div>
+      <RickAndMorty />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/RickAndMorty.jsx
@@ -0,0 +1,83 @@
+import React, { useEffect, useState, useRef } from "react";
+import "../styles/cartoon.css";
+
+function RickAndMorty() {
+  const [cartoon, setCartoon] = useState([]);
+  const currentPageRef = useRef(1);
+
+  useEffect(() => {
+    fetchCartoon();
+  }, []);
+
+  async function fetchCartoon() {
+    try {
+      const response = await fetch(
+        `https://rickandmortyapi.com/api/character?page=${currentPageRef.current}`
+      );
+      const data = await response.json();
+      setCartoon(data.results);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  function next() {
+    currentPageRef.current += 1;
+    fetchCartoon();
+  }
+
+  function prev() {
+    if (currentPageRef.current > 1) {
+      currentPageRef.current -= 1;
+      fetchCartoon();
+    }
+  }
+
+  return (
+    <div className="container">
+      {cartoon.map((info) => (
+        <div className="card" key={info.id}>
+          <div id="ProfilePic">
+            <div id="img">
+              <img src={info.image} />
+            </div>
+          </div>
+          <div id="profileInfo">
+            <p>
+              <b>Name: </b>
+              {info.name}
+            </p>
+            <p>
+              <b>Status: </b>
+              {info.status}
+            </p>
+            <p>
+              <b>Species: </b>
+              {info.species}
+            </p>
+            <p>
+              <b>Gender: </b>
+              {info.gender}
+            </p>
+            <p>
+              <b>Origin name:</b>
+              {info.origin.name}
+            </p>
+            <p>
+              <b>Location name:</b>
+              {info.location.name}
+            </p>
+          </div>
+        </div>
+      ))}
+      <div className="NavigationPanel">
+        <button onClick={prev} disabled={currentPageRef.current === 1}>
+          Prev
+        </button>
+        <button onClick={next}>Next</button>
+      </div>
+    </div>
+  );
+}
+
+export default RickAndMorty;
